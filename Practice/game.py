80404c060ba6f505f420f4aeb1dd756eb04ed2df
Artas124 <artem_babynin@mail.ru>
2024-12-25 22:49:48 +0300

    Add files via upload

---
+++ Practice/game.py
@@ -0,0 +1,73 @@
+from modules.manager import FileManager
+from modules.logic import GameLogic
+
+file_manager = FileManager()
+words = file_manager.load_words()
+record = file_manager.load_record()
+game = GameLogic(words)
+
+while True:
+    hidden_word = game.start_new_game()
+    if hidden_word is None:
+        print("Список слов пуст. Завершение игры.")
+        break
+
+    difficulty = input("Выберите режим (легкий, средний, сложный): ").lower()
+    if difficulty == "легкий":
+        lives = 7
+    elif difficulty == "средний":
+        lives = 5
+    elif difficulty == "сложный":
+        lives = 3
+    else:
+        print("Неверный режим. Игра будет запущена на легком уровне.")
+        lives = 7
+
+    print("Угадайте слово:", "".join(hidden_word))
+
+    while lives > 0 and not game.game_over(hidden_word):
+        try:
+            guess = input("Введите букву или слово целиком: ").upper()
+
+
+            if len(guess) > 1:
+                if guess == game.get_word():
+                    hidden_word = list(game.get_word())
+                    print("Вы угадали слово!")
+                    break
+                else:
+                    lives = 0
+                    print("Неверно! Вы потеряли все жизни.")
+                    break
+
+
+            result = game.make_guess(guess, hidden_word)
+            if result is False:
+                lives -= 1
+                print(f"Неверно. Осталось {lives} жизней.")
+            elif isinstance(result, str):
+                hidden_word = result
+                print(hidden_word)
+
+        except Exception as e:
+            print(f"Произошла непредвиденная ошибка: {e}")
+
+        if game.game_over(hidden_word):
+            print(f"Вы угадали слово {game.get_word()}!")
+            record += 1
+            file_manager.save_record(record)
+            break
+
+        if not game.words:
+            print("Все слова угаданы!")
+            break
+
+    if lives == 0:
+        print(f'Ваш счёт: {record}. Игра окончена!')
+        exit()
+    else:
+        print(f"Ваш счёт: {record}")
+        play_again = input("Хотите сыграть еще раз? (да/нет): ").lower()
+        if play_again == 'нет':
+            print('Игра окончена!')
+            exit()
